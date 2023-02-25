9fce325787b06530115f554b3fae978fb91a1bbf
Suhail Thakrani <57296007+suhailthakrani@users.noreply.github.com>
2023-02-25 15:13:20 +0500

    updated ui!

--- lib/screens/authentication/login_screen.dart
+++ lib/screens/authentication/login_screen.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:get/get.dart';
+
 import 'package:suit_up/constants/colors.dart';
+import 'package:suit_up/controllers/asset_manager.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -12,6 +15,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _passController = TextEditingController();
+  final _emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
@@ -25,20 +31,33 @@ class _LoginScreenState extends State<LoginScreen> {
             const Text(
               'Hello Again!',
               style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w700,
+                fontSize: 28,
+                fontWeight: FontWeight.w500,
               ),
             ),
             SizedBox(height: height * 0.05),
-            const Text(
-              "Welcome Back you've been missed",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                "Welcome Back you've been missed",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 24,
+                    // fontWeight: FontWeight.w600,
+                    wordSpacing: 0.3,
+                    letterSpacing: 0.3),
               ),
             ),
-            TextFormField(),
-            TextFormField(),
+            SizedBox(height: 10),
+            CustomTextFeild(
+              controller: _emailController,
+            ),
+            SizedBox(height: 10),
+            CustomTextFeild(
+              controller: _passController,
+              suffix: Icon(Icons.visibility),
+            ),
+            SizedBox(height: 16),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: const [
@@ -47,6 +66,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 )
               ],
             ),
+            SizedBox(height: 10),
             SizedBox(
               width: MediaQuery.of(context).size.width * 0.8,
               child: ElevatedButton(
@@ -56,6 +76,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
               ),
             ),
+            SizedBox(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -72,25 +93,75 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
               ],
             ),
+            SizedBox(height: 16),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 ElevatedButton(
                   onPressed: () {},
-                  child: const Icon(Icons.face),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                  ),
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    child: Image.asset(AssetManager.googleLogo),
+                  ),
                 ),
                 ElevatedButton(
                   onPressed: () {},
-                  child: const Icon(Icons.face),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                  ),
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    child: Image.asset(AssetManager.googleLogo),
+                  ),
                 ),
                 ElevatedButton(
                   onPressed: () {},
-                  child: const Icon(Icons.face),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                  ),
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    child: Image.asset(AssetManager.googleLogo),
+                  ),
                 )
               ],
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CustomTextFeild extends StatelessWidget {
+  final TextEditingController controller;
+  final Widget suffix;
+
+  final double height;
+  const CustomTextFeild({
+    Key? key,
+    required this.controller,
+    this.suffix = const SizedBox(),
+    this.height = 45,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+            suffixIcon: suffix,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+            )),
       ),
     );
   }
