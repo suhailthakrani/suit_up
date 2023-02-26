1ad357eef4306c82906425f05b445c48f3200e8e
Suhail Thakrani <57296007+suhailthakrani@users.noreply.github.com>
2023-02-26 20:38:23 +0500

    Almost completed homescreen!

--- lib/controllers/asset_manager.dart
+++ lib/controllers/asset_manager.dart
@@ -5,4 +5,10 @@ class AssetManager {
   static const facebookLogo = '$images/facebook.png';
   static const googleLogo = '$images/google.png';
   static const appleLogo = '$images/apple.png';
+  static const cardImage = '$images/card_image.jpg';
+  static const card1 = '$images/c1.jpg';
+  static const card2 = '$images/c2.jpg';
+  static const card3 = '$images/c3.jpg';
+  static const card4 = '$images/c4.jpg';
+  static const card5 = '$images/c5.jpg';
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:suit_up/controllers/asset_manager.dart';
+import 'package:suit_up/screens/authentication/sign_up_screen.dart';
+import 'package:suit_up/theme/theme_constants.dart';
 import 'package:suit_up/theme/theme_manager.dart';
 import 'package:suit_up/widgets/custom_textfeild.dart';
 
@@ -14,26 +17,168 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final _searchController = TextEditingController();
-   final ThemeManager _themeManager = ThemeManager();
-   
+  final ThemeManager _themeManager = ThemeManager();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      // backgroundColor: AppTheme.lightTheme.scaffoldBackgroundColor,
       body: SafeArea(
-        child: Column(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
           children: [
-            CupertinoTextField(
-              placeholder: 'Type here',
-              controller: _searchController,
-              decoration:
-                  BoxDecoration(borderRadius: BorderRadius.circular(12)),
+            Row(
+              children: [
+                Expanded(
+                  child: SizedBox(
+                    height: 50,
+                    child: CupertinoTextField(
+                      placeholder: 'Type here',
+                      prefix: const Padding(
+                        padding: EdgeInsets.only(left: 5.0),
+                        child: Icon(
+                          CupertinoIcons.search,
+                          color: Colors.purple,
+                        ),
+                      ),
+                      controller: _searchController,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade200,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+                WidthCustom(10),
+                SizedBox(
+                  height: 50,
+                  width: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      backgroundColor: Colors.grey.shade300,
+                      elevation: 1,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.notifications,
+                      size: 35,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            HeightCustom(10),
+            SizedBox(
+              height: 130,
+              child: Card(
+                color: Colors.amber,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    AssetManager.cardImage,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              ),
+            ),
+            HeightCustom(16),
+            Container(
+              height: 50,
+              child: ListView.separated(
+                itemCount: 5,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                            5,
+                          ),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text('Category'));
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return WidthCustom(5);
+                },
+              ),
+            ),
+            HeightCustom(16),
+            Container(
+              height: 500,
+              child: GridView(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+                children: [
+                  Card(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        AssetManager.card1,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        AssetManager.card2,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        AssetManager.card3,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        AssetManager.card4,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        AssetManager.card1,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        AssetManager.card2,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             )
           ],
         ),
-
       ),
-     
     );
   }
 }
