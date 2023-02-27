6963acdd3e9c1e0579e8a0f4c83ca85855314460
Suhail Thakrani <57296007+suhailthakrani@users.noreply.github.com>
2023-02-27 18:26:36 +0500

    Added coursel slider in home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -40,99 +41,103 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       // backgroundColor: AppTheme.lightTheme.scaffoldBackgroundColor,
+
+      appBar: AppBar(
+        toolbarHeight: 60,
+        title: CupertinoTextField(
+          placeholder: 'Type here',
+          prefix: const Padding(
+            padding: EdgeInsets.only(left: 5.0),
+            child: Icon(
+              CupertinoIcons.search,
+              color: Colors.purple,
+            ),
+          ),
+          controller: _searchController,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade200,
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        actions: [
+          SizedBox(
+            height: 40,
+            width: 40,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.zero,
+                backgroundColor: Colors.grey.shade300,
+                elevation: 1,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              onPressed: () {},
+              child: const Icon(
+                Icons.notifications_outlined,
+                size: 30,
+              ),
+            ),
+          ),
+          WidthCustom(10),
+        ],
+      ),
       body: SafeArea(
-        child: Column(
-          // shrinkWrap: true,
-          // padding: EdgeInsets.all(16),
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: SizedBox(
-                    height: 50,
-                    child: CupertinoTextField(
-                      placeholder: 'Type here',
-                      prefix: const Padding(
-                        padding: EdgeInsets.only(left: 5.0),
-                        child: Icon(
-                          CupertinoIcons.search,
-                          color: Colors.purple,
-                        ),
-                      ),
-                      controller: _searchController,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade200,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                    ),
-                  ),
-                ),
-                //Good Morning!
-                WidthCustom(10),
-                SizedBox(
-                  height: 50,
-                  width: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      backgroundColor: Colors.grey.shade300,
-                      elevation: 1,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    onPressed: () {},
-                    child: const Icon(
-                      Icons.notifications_outlined,
-                      size: 30,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            HeightCustom(10),
-            Card(
-              color: Colors.amber,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: Image.asset(
-                  AssetManager.cardImage,
-                  fit: BoxFit.fitWidth,
-                ),
-              ),
-            ),
-            HeightCustom(16),
-            Flexible(
-              child: GridView.builder(
-                itemCount: productsModel.products.isNotEmpty
-                    ? productsModel.products.length
-                    : 0,
-                shrinkWrap: true,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2),
-                itemBuilder: (context, index) {
-                  return Column(
-                    children: [
-                      Card(
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(12),
-                          child: Image.asset(
-                            productsModel.products[index].imageUrl ?? '',
-                            height: 130,
-                            fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              CarouselSlider(
+                  options: CarouselOptions(height: 150),
+                  items: [1, 2, 3, 4, 5].map((e) {
+                    return Builder(
+                      builder: (context) {
+                        return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.all(8),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.asset(
+                              AssetManager.cardImage,
+                              height: 120,
+                              fit: BoxFit.fitWidth,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }).toList()),
+              HeightCustom(16),
+              Container(
+                height: 600,
+                child: GridView.builder(
+                  itemCount: productsModel.products.isNotEmpty
+                      ? productsModel.products.length
+                      : 0,
+                  shrinkWrap: true,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    return Stack(
+                      children: [
+                        Card(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.asset(
+                              productsModel.products[index].imageUrl ?? '',
+                              // height: 130,
+                              fit: BoxFit.fitWidth,
+                            ),
                           ),
                         ),
-                      ),
-                      Text('Rs. ${productsModel.products[index].price}'),
-                    ],
-                  );
-                },
-              ),
-            )
-          ],
+                        Text('Rs. ${productsModel.products[index].price}'),
+                      ],
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
