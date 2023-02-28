5f0f13f88195e81dffad7f24505016dee936f98d
Suhail Thakrani <57296007+suhailthakrani@users.noreply.github.com>
2023-02-28 09:17:21 +0500

    Solved Scroll Issue and added more products

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -82,62 +82,62 @@ class _HomeScreenState extends State<HomeScreen> {
           WidthCustom(10),
         ],
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: [
-              CarouselSlider(
-                  options: CarouselOptions(height: 150),
-                  items: [1, 2, 3, 4, 5].map((e) {
-                    return Builder(
-                      builder: (context) {
-                        return Container(
-                          width: MediaQuery.of(context).size.width,
-                          margin: const EdgeInsets.all(8),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
-                            child: Image.asset(
-                              AssetManager.cardImage,
-                              height: 120,
-                              fit: BoxFit.fitWidth,
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  }).toList()),
-              HeightCustom(16),
-              SizedBox(
-                height: 600,
-                child: GridView.builder(
-                  itemCount: productsModel.products.isNotEmpty
-                      ? productsModel.products.length
-                      : 0,
-                  shrinkWrap: true,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                  itemBuilder: (context, index) {
-                    return Stack(
-                      children: [
-                        Card(
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
-                            child: Image.asset(
-                              productsModel.products[index].imageUrl ?? '',
-                              // height: 130,
-                              fit: BoxFit.fitWidth,
-                            ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CarouselSlider(
+                options: CarouselOptions(height: 150),
+                items: [1, 2, 3, 4, 5].map((e) {
+                  return Builder(
+                    builder: (context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.all(8),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image.asset(
+                            AssetManager.cardImage,
+                            height: 120,
+                            fit: BoxFit.fitWidth,
                           ),
                         ),
-                        Text('Rs. ${productsModel.products[index].price}'),
-                      ],
-                    );
-                  },
-                ),
-              )
-            ],
-          ),
+                      );
+                    },
+                  );
+                }).toList()),
+            HeightCustom(16),
+            Flexible(
+              // height: 600,
+              child: GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: productsModel.products.isNotEmpty
+                    ? productsModel.products.length
+                    : 0,
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  return Stack(
+                    children: [
+                      Card(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image.asset(
+                            productsModel.products[index].imageUrl ?? '',
+                            // height: 130,
+                            fit: BoxFit.fitWidth,
+                          ),
+                        ),
+                      ),
+                      Text('Rs. ${productsModel.products[index].price}'),
+                    ],
+                  );
+                },
+              ),
+            )
+          ],
         ),
       ),
     );
