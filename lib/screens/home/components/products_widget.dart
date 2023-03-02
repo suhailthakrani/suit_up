a4a4613188b824848c593c1d1d5f4c2323572429
Suhail Thakrani <57296007+suhailthakrani@users.noreply.github.com>
2023-03-02 10:51:49 +0500

    Improve code readibility and added product card!

--- lib/screens/home/components/products_widget.dart
+++ lib/screens/home/components/products_widget.dart
@@ -1,10 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 import 'package:suit_up/models/products_model.dart';
 import 'package:suit_up/screens/authentication/sign_up_screen.dart';
 import 'package:suit_up/screens/home/components/product_screen.dart';
 import 'package:suit_up/utils/dimensions.dart';
 import 'package:suit_up/widgets/custom_text.dart';
+import 'package:suit_up/widgets/product_card.dart';
 
 class ProductsWidget extends StatelessWidget {
   const ProductsWidget({
@@ -17,10 +20,13 @@ class ProductsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
+      shrinkWrap: true,
       physics: const BouncingScrollPhysics(),
       itemCount:
           productsModel.products.isNotEmpty ? productsModel.products.length : 0,
-      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return ProductCard(product: productsModel.products[index]);
+      },
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount:
             MediaQuery.of(context).orientation == Orientation.portrait ? 2 : 3,
@@ -28,147 +34,6 @@ class ProductsWidget extends StatelessWidget {
         crossAxisSpacing: Dimensions.width10,
         mainAxisSpacing: Dimensions.width10,
       ),
-      itemBuilder: (context, index) {
-        return Container(
-          // color: Colors.amber,
-          alignment: Alignment.center,
-          child: Stack(
-            children: [
-              Column(
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ProductScreen(
-                              product: productsModel.products[index]),
-                        ),
-                      );
-                    },
-                    child: Card(
-                      color: Colors.amber,
-                      // margin: index % 2 == 0
-                      //     ? EdgeInsets.only(left: Dimensions.height16)
-                      //     : EdgeInsets.only(
-                      //         right: Dimensions.width10,
-                      //         left: Dimensions.width5),
-                      child: ClipRRect(
-                        borderRadius:
-                            BorderRadius.circular(Dimensions.radius12),
-                        child: Image.asset(
-                          productsModel.products[index].imageUrl ?? '',
-                          height: Dimensions.height220,
-                          width: MediaQuery.of(context).size.width * 0.45,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                  HeightCustom(Dimensions.height8),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                productsModel.products[index].name ?? '',
-                                softWrap: true,
-                                style: TextStyle(
-                                  overflow: TextOverflow.ellipsis,
-                                  color: Colors.purple,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: Dimensions.font18,
-                                ),
-                              ),
-                              TextCustom(
-                                text:
-                                    'Rs. ${productsModel.products[index].price}',
-                                color: Colors.grey.shade900,
-                                fontWeight: FontWeight.w500,
-                                fontSize: Dimensions.font13,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          height: Dimensions.height45,
-                          width: Dimensions.height45,
-                          decoration: BoxDecoration(
-                            borderRadius:
-                                BorderRadius.circular(Dimensions.radius40),
-                            color: Colors.grey.shade200,
-                          ),
-                          child: IconButton(
-                              onPressed: () {},
-                              style: IconButton.styleFrom(
-                                padding: EdgeInsets.zero,
-                              ),
-                              icon: Icon(
-                                CupertinoIcons.cart,
-                                size: Dimensions.iconSize24,
-                                color: Colors.purple,
-                              )),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Positioned(
-                top: 4,
-                right: index % 2 == 0
-                    ? Dimensions.width10 - 4
-                    : Dimensions.height10,
-                child: Container(
-                  alignment: Alignment.center,
-                  height: Dimensions.height30,
-                  width: Dimensions.height60,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(Dimensions.radius12),
-                      topRight: Radius.circular(Dimensions.radius12),
-                    ),
-                    color: Colors.redAccent,
-                  ),
-                  child: TextCustom(
-                    text: "${productsModel.products[index].discount} % OFF",
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                    fontSize: Dimensions.font12,
-                  ),
-                ),
-              ),
-              Positioned(
-                top: Dimensions.width5,
-                left: Dimensions.height16,
-                child: Container(
-                  height: Dimensions.height30 + 2,
-                  width: Dimensions.height30 + 2,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(Dimensions.radius40),
-                    color: Colors.grey.shade200,
-                  ),
-                  child: IconButton(
-                      onPressed: () {},
-                      style: IconButton.styleFrom(
-                        padding: EdgeInsets.zero,
-                      ),
-                      icon: Icon(
-                        CupertinoIcons.heart,
-                        size: Dimensions.iconSize24,
-                        color: Colors.purple,
-                      )),
-                ),
-              ),
-            ],
-          ),
-        );
-      },
     );
   }
 }
---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:suit_up/models/products_model.dart';
+import 'package:suit_up/screens/authentication/sign_up_screen.dart';
+import 'package:suit_up/screens/home/components/product_screen.dart';
+import 'package:suit_up/utils/dimensions.dart';
+import 'package:suit_up/widgets/custom_text.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Products product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // color: Colors.amber,
+      alignment: Alignment.center,
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ProductScreen(product: product),
+                    ),
+                  );
+                },
+                child: Card(
+                  color: Colors.amber,
+                  // margin: index % 2 == 0
+                  //     ? EdgeInsets.only(left: Dimensions.height16)
+                  //     : EdgeInsets.only(
+                  //         right: Dimensions.width10,
+                  //         left: Dimensions.width5),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(Dimensions.radius12),
+                    child: Image.asset(
+                      product.imageUrl ?? '',
+                      height: Dimensions.height220,
+                      width: MediaQuery.of(context).size.width * 0.45,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              HeightCustom(Dimensions.height8),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product.name ?? '',
+                            softWrap: true,
+                            style: TextStyle(
+                              overflow: TextOverflow.ellipsis,
+                              color: Colors.purple,
+                              fontWeight: FontWeight.w500,
+                              fontSize: Dimensions.font18,
+                            ),
+                          ),
+                          TextCustom(
+                            text: 'Rs. ${product.price}',
+                            color: Colors.grey.shade900,
+                            fontWeight: FontWeight.w500,
+                            fontSize: Dimensions.font13,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: Dimensions.height45,
+                      width: Dimensions.height45,
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(Dimensions.radius40),
+                        color: Colors.grey.shade200,
+                      ),
+                      child: IconButton(
+                          onPressed: () {},
+                          style: IconButton.styleFrom(
+                            padding: EdgeInsets.zero,
+                          ),
+                          icon: Icon(
+                            CupertinoIcons.cart,
+                            size: Dimensions.iconSize24,
+                            color: Colors.purple,
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: 4,
+            right: Dimensions.height10 - 2,
+            child: Container(
+              alignment: Alignment.center,
+              height: Dimensions.height30,
+              width: Dimensions.height60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(Dimensions.radius12),
+                  topRight: Radius.circular(Dimensions.radius12),
+                ),
+                color: Colors.redAccent,
+              ),
+              child: TextCustom(
+                text: "${product.discount} % OFF",
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontSize: Dimensions.font12,
+              ),
+            ),
+          ),
+          Positioned(
+            top: Dimensions.width5,
+            left: Dimensions.height16,
+            child: Container(
+              height: Dimensions.height30 + 2,
+              width: Dimensions.height30 + 2,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimensions.radius40),
+                color: Colors.grey.shade200,
+              ),
+              child: IconButton(
+                  onPressed: () {},
+                  style: IconButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                  ),
+                  icon: Icon(
+                    CupertinoIcons.heart,
+                    size: Dimensions.iconSize24,
+                    color: Colors.purple,
+                  )),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
