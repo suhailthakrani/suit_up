5459edb3c494cc42ca523b40011be3c9110e6710
Suhail Thakrani <57296007+suhailthakrani@users.noreply.github.com>
2023-03-02 09:23:34 +0500

    Added dimensions to avoid overflows and to have responsive ui!

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -1,7 +1,131 @@
 import 'package:get/get.dart';
 
 class Dimensions {
-  static double height = Get.height;
-  static double width = Get.width;
-  static double height1 = height/10;
+  static double screenHight = Get.height; // 772.0
+  static double screenWidth = Get.width; // 360.0
+
+  static double mainContainerWidth_0_40 = screenWidth * 0.4;
+  static double pageViewContainer = screenHight / 3.106493506494155; // 220
+  static double pageViewTextContainer = screenHight / 5.695238095239283; // 120
+  static double pageView = screenHight / 2.135714285714731; // 320
+
+// dynamic height
+  static double height1 = screenHight / 772.0;
+  static double height2 = height1 * 2;
+  static double height3 = height1 * 3;
+  static double height5 = height1 * 5;
+  static double height8 = height1 * 8;
+  static double height10 = height1 * 10;
+  static double height11 = height1 * 11;
+  static double height12 = height1 * 12;
+  static double height16 = height10 * 1.6;
+  static double height20 = height10 * 2;
+  static double height24 = height10 * 2.4;
+  static double height25 = height10 * 2.5;
+  static double height28 = height10 * 2.8;
+  static double height30 = height10 * 3;
+  static double height35 = height10 * 3.5;
+  static double height38 = height10 * 3.8;
+  static double height45 = height10 * 4.5;
+
+  static double height40 = height10 * 4.0;
+  static double height50 = height10 * 5.0;
+  static double height60 = height10 * 6.0;
+  static double height65 = height10 * 6.5;
+  static double height70 = height10 * 7.0;
+  static double height80 = height10 * 8.0;
+
+  static double height90 = height1 * 90;
+  static double height100 = height1 * 100;
+  static double height110 = height1 * 110;
+  static double height120 = height1 * 120;
+  static double height130 = height1 * 130;
+  static double height135 = height1 * 135;
+  static double height140 = height1 * 140;
+  static double height150 = height1 * 150;
+  static double height160 = height1 * 160;
+  static double height180 = height1 * 180;
+  static double height200 = height1 * 200;
+  static double height220 = height1 * 220;
+  static double height250 = height1 * 250;
+  static double height300 = height1 * 300;
+  static double height350 = height1 * 350;
+
+  // dynamic width
+  static double width1 = screenWidth / 360.0;
+
+  static double width2 = width1 * 2;
+  static double width5 = width1 * 5;
+  static double width10 = width1 * 10;
+  static double width12 = width1 * 12;
+  static double width16 = width10 * 1.6;
+  static double width20 = width10 * 2;
+  static double width30 = width10 * 3;
+  static double width35 = width10 * 3.5;
+  static double width40 = width10 * 4;
+
+  static double width45 = width10 * 4.5;
+
+  static double width70 = width10 * 7;
+  static double width80 = width10 * 8;
+
+  static double width90 = width10 * 9;
+  static double width100 = width1 * 120;
+  static double width120 = width1 * 120;
+  static double width130 = width1 * 130;
+  static double width150 = width1 * 150;
+  static double width180 = width1 * 180;
+  static double width200 = width1 * 200;
+  static double width250 = width1 * 250;
+
+  static double width300 = width1 * 300;
+  static double width400 = width1 * 400;
+
+  static double font1 = height1;
+  static double font8 = height8;
+  static double font9 = height90 / 10;
+  static double font10 = height10;
+  static double font11 = height1 * 11;
+  static double font12 = height1 * 12;
+  static double font13 = height1 * 13;
+  static double font14 = height1 * 14;
+  static double font15 = height1 * 15;
+  static double font16 = height1 * 16;
+  static double font17 = height1 * 17;
+  static double font18 = height1 * 18;
+  static double font20 = height20;
+  static double font24 = height24;
+  static double font26 = font1 * 26;
+
+  static double radius10 = height10;
+
+  static double radius5 = height10 / 2;
+  static double radius8 = height8;
+  static double radius12 = height12;
+  static double radius16 = height1 * 16;
+  static double radius20 = height20;
+  static double radius22 = height1 * 22;
+  static double radius24 = height24;
+  static double radius26 = height2 * 13;
+  static double radius28 = height28;
+  static double radius30 = height30;
+  static double radius40 = height40;
+  static double radius50 = height50;
+
+  static double iconSize22 = height10 * 2.2;
+  static double iconSize24 = height10 * 2.4;
+  static double iconSize28 = height10 * 2.8;
+  static double iconSize30 = height10 * 3;
+
+  static double mainHomeContainerWidth = screenWidth * 0.40;
+
+  // Icon size
+  static double iconSize12 = height1 * 12;
+  static double iconSize14 = height1 * 14;
+  static double iconSize16 = height1 * 16;
+  static double iconSize18 = height1 * 18;
+  static double iconSize20 = height1 * 20;
+
+  // Bottom height
+  static double bottomHeightBar = height1 * 120;
 }
